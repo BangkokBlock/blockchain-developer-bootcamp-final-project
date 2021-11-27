05b658546f10fb8b8214fed1cd870d759823e7b0
Mark Tellez (Devmentor.live) <mark@devmentor.live>
2021-11-27 16:17:03 -0600

    a few fixes to  contract price and some basic code fixes for mktplc

--- pages/index.js
+++ pages/index.js
@@ -1,92 +1,115 @@
-import { ethers } from 'ethers'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import Web3Modal from 'web3modal'
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
 
-
-import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
-import MVMarket from '../artifacts/contracts/MVMarket.sol/MVMarket.json'
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import MVMarket from "../artifacts/contracts/MVMarket.sol/MVMarket.json";
 
 export default function Home() {
-  const [nfts, setNFts] = useState([])
-  const [loadingState, setLoadingState] = useState('not-loaded')
+  const [nfts, setNFts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
 
   useEffect(() => {
-    loadNFTs()
-  }, [])
+    loadNFTs();
+  }, []);
 
   async function loadNFTs() {
     // what we want to load:
     // ***provider, tokenContract, marketContract, data for our marketItems***
-    const provider = new ethers.providers.JsonRpcProvider()
-    const tokenContract = new ethers.Contract(process.env.TOKEN_ADDRESS, NFT.abi, provider)
-    const marketContract = new ethers.Contract(process.env.MARKET_ADDRESS, MVMarket.abi, provider)
-    const data = await marketContract.getMarketTokens()
+    const provider = new ethers.providers.JsonRpcProvider(
+      process.env.NEXT_PUBLIC_INFURA_URL
+    );
+    const tokenContract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_TOKEN_ADDRESS,
+      NFT.abi,
+      provider
+    );
+    const marketContract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_MARKET_ADDRESS,
+      MVMarket.abi,
+      provider
+    );
+    const data = await marketContract.getMarketTokens();
+    console.log("data", data);
 
-    const items = await Promise.all(data.map(async i => {
-      const tokenUri = await tokenContract.tokenURI(i.tokenId)
-      // we want get the token metadata - json 
-      const meta = await axios.get(tokenUri)
-      const price = ethers.utils.formatUnits(i.price.toString(), 'ether')
-      const item = {
-        price,
-        tokenId: i.tokenId.toNumber(),
-        seller: i.seller,
-        owner: i.owner,
-        image: meta.data.image,
-        name: meta.data.name,
-        description: meta.data.description
-      }
-      return item
-    }))
-    setNFts(items)
-    setLoadingState('loaded')
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenId);
+        // we want get the token metadata - json
+        const meta = await axios.get(tokenUri);
+        const price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        const item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+    setNFts(items);
+    setLoadingState("loaded");
   }
 
-  // function to buy nfts for market 
+  // function to buy nfts for market
   async function buyNFT(nft) {
-    const web3Modal = new Web3Modal()
-    const connection = await web3Modal.connect()
-    const provider = new ethers.providers.Web3Provider(connection)
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(process.env.TOKEN_ADDRESS, MVMarket.abi, signer)
-    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
-    const transaction = await contract.createMarketSale(process.env.TOKEN_ADDRESS, nft.tokenId, {
-      value: price
-    })
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_TOKEN_ADDRESS,
+      MVMarket.abi,
+      signer
+    );
+    const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+    const transaction = await contract.createMarketSale(
+      process.env.NEXT_PUBLIC_TOKEN_ADDRESS,
+      nft.tokenId,
+      {
+        value: price,
+      }
+    );
 
-    await transaction.wait()
-    loadNFTs()
+    await transaction.wait();
+    loadNFTs();
   }
-  if (loadingState === 'loaded' && !nfts.length) return (<h1
-    className='px-20 py-7 text-4x1'>No NFts in marketplace</h1>)
+  if (loadingState === "loaded" && !nfts.length)
+    return <h1 className="px-20 py-7 text-4x1">No NFts in marketplace</h1>;
 
   return (
-    <div className='flex justify-center'>
-      <div className='px-4' style={{ maxWidth: '1600px' }}>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
-          {
-            nfts.map((nft, i) => (
-              <div key={i} className='border shadow rounded-x1 overflow-hidden'>
-                <img src={nft.image} />
-                <div className='p-4'>
-                  <p style={{ height: '64px' }} className='text-3x1 font-bold'>{
-                    nft.name}</p>
-                  <div style={{ height: '72px', overflow: 'hidden' }}>
-                    <p className='text-black-400'>{nft.description}</p>
-                  </div>
-                </div>
-                <div className='p-4 bg-black'>
-                  <p className='text-3x-1 mb-4 font-bold text-white'>{nft.price} ETH</p>
-                  <button className='w-full bg-purple-500 text-white font-bold py-3 px-12 rounded'
-                    onClick={() => buyNFT(nft)} >Buy
-                  </button>
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: "1600px" }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-x1 overflow-hidden">
+              <img src={nft.image} />
+              <div className="p-4">
+                <p style={{ height: "64px" }} className="text-3x1 font-bold">
+                  {nft.name}
+                </p>
+                <div style={{ height: "72px", overflow: "hidden" }}>
+                  <p className="text-black-400">{nft.description}</p>
                 </div>
               </div>
-            ))
-          }
+              <div className="p-4 bg-black">
+                <p className="text-3x-1 mb-4 font-bold text-white">
+                  {nft.price} ETH
+                </p>
+                <button
+                  className="w-full bg-purple-500 text-white font-bold py-3 px-12 rounded"
+                  onClick={() => buyNFT(nft)}>
+                  Buy
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
--- pages/mint-item.js
+++ pages/mint-item.js
@@ -1,111 +1,132 @@
-import { ethers } from 'ethers'
-import { useState } from 'react'
-import Web3Modal from 'web3modal'
-import { create as ipfsHttpClient } from 'ipfs-http-client'
+import { ethers } from "ethers";
+import { useState } from "react";
+import Web3Modal from "web3modal";
+import { create as ipfsHttpClient } from "ipfs-http-client";
 
-import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
-import { useRouter } from 'next/router'
-import MVMarket from '../artifacts/contracts/MVMarket.sol/MVMarket.json'
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import { useRouter } from "next/router";
+import MVMarket from "../artifacts/contracts/MVMarket.sol/MVMarket.json";
 
 // in this component we set the ipfs up to host our nft data of
-// file storage 
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+// file storage
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
 export default function MintItem() {
-    const [fileUrl, setFileUrl] = useState(null)
-    const [formInput, updateFormInput] = useState({ price: '', name: '', description: '' })
-    const router = useRouter()
+  const [fileUrl, setFileUrl] = useState(null);
+  const [formInput, updateFormInput] = useState({
+    price: "",
+    name: "",
+    description: "",
+  });
+  const router = useRouter();
 
-    async function onChange(e) {
-        const file = e.target.files[0]
-        try {
-            const added = await client.add(
-                file, {
-                progress: (prog) => console.log(`received: ${prog}`)
-            })
-            const url = `https://ipfs.infura.io/ipfs/${added.path}`
-            setFileUrl(url)
-        } catch (error) {
-            console.log('Error uploading file:', error)
-        }
+  async function onChange(e) {
+    const file = e.target.files[0];
+    try {
+      const added = await client.add(file, {
+        progress: (prog) => console.log(`received: ${prog}`),
+      });
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      setFileUrl(url);
+    } catch (error) {
+      console.log("Error uploading file:", error);
     }
+  }
 
-    async function createMarket() {
-        const { name, description, price } = formInput
-        if (!name || !description || !price || !fileUrl) return
-        // upload to IPFS
-        const data = JSON.stringify({
-            name, description, image: fileUrl
-        })
-        try {
-            const added = await client.add(data)
-            const url = `https://ipfs.infura.io/ipfs/${added.path}`
-            // run a function that creates sale and passes in the url 
-            createSale(url)
-        } catch (error) {
-            console.log('Error uploading file:', error)
-        }
+  async function createMarket() {
+    const { name, description, price } = formInput;
+    if (!name || !description || !price || !fileUrl) return;
+    // upload to IPFS
+    const data = JSON.stringify({
+      name,
+      description,
+      image: fileUrl,
+    });
+    try {
+      const added = await client.add(data);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      // run a function that creates sale and passes in the url
+      createSale(url);
+    } catch (error) {
+      console.log("Error uploading file:", error);
     }
+  }
 
-    async function createSale(url) {
-        // create the items and list them on the marketplace
-        const web3Modal = new Web3Modal()
-        const connection = await web3Modal.connect()
-        const provider = new ethers.providers.Web3Provider(connection)
-        const signer = provider.getSigner()
+  async function createSale(url) {
+    // this stuff has to be server-side
+    // create the items and list them on the marketplace
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
 
-        // we want to create the token
-        let contract = new ethers.Contract(process.env.TOKEN_ADDRESS, NFT.abi, signer)
-        let transaction = await contract.mintToken(url)
-        let tx = await transaction.wait()
-        let event = tx.events[0]
-        let value = event.args[2]
-        let tokenId = value.toNumber()
-        const price = ethers.utils.parseUnits(formInput.price, 'ether')
+    // we want to create the token
+    const tokenContract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_TOKEN_ADDRESS,
+      NFT.abi,
+      signer
+    );
+    const mintTx = await tokenContract.mintToken(url);
+    console.dir(mintTx);
+    const event = (await mintTx.wait()).events[0];
+    const value = event.args[2];
+    const tokenId = value.toNumber();
+    const price = ethers.utils.parseUnits(formInput.price, "ether");
 
-        // list the item for sale on the marketplace 
-        contract = new ethers.Contract(process.env.MARKET_ADDRESS, MVMarket.abi, signer)
-        let listingPrice = await contract.getListingPrice()
-        listingPrice = listingPrice.toString()
+    // list the item for sale on the marketplace
+    const marketContract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_MARKET_ADDRESS,
+      MVMarket.abi,
+      signer
+    );
+    const mktListingPrice = (await marketContract.getListingPrice()).toString();
+    console.log("mktListingPrice", mktListingPrice);
 
-        transaction = await contract.makeMarketItem(process.env.TOKEN_ADDRESS, tokenId, price, { value: listingPrice })
-        await transaction.wait()
-        router.push('./')
-    }
+    const makeMktItemTx = await marketContract.makeMarketItem(
+      process.env.NEXT_PUBLIC_TOKEN_ADDRESS,
+      tokenId,
+      price,
+      { value: mktListingPrice }
+    );
+    console.dir(makeMktItemTx);
+    await makeMktItemTx.wait();
+    router.push("./");
+  }
 
-    return (
-        <div className='flex justify-center'>
-            <div className='w-1/2 flex flex-col pb-12'>
-                <input
-                    placeholder='Artist Name'
-                    className='mt-8 border rounded p-4'
-                    onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
-                />
-                <textarea
-                    placeholder='Performance Date'
-                    className='mt-2 border rounded p-4'
-                    onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
-                />
-                <input
-                    placeholder='NFT Price in Eth'
-                    className='mt-2 border rounded p-4'
-                    onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
-                />
-                <input
-                    type='file'
-                    name='Artist'
-                    className='mt-4'
-                    onChange={onChange}
-                /> {
-                    fileUrl && (
-                        <img className='rounded mt-4' width='350px' src={fileUrl} />
-                    )}
-                <button onClick={createMarket}
-                    className='font-bold mt-4 bg-purple-500 text-white rounded p-4 shadow-lg'
-                >
-                    Mint NFT
-                </button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2 flex flex-col pb-12">
+        <input
+          placeholder="Artist Name"
+          className="mt-8 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, name: e.target.value })
+          }
+        />
+        <textarea
+          placeholder="Performance Date"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, description: e.target.value })
+          }
+        />
+        <input
+          placeholder="NFT Price in Eth"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, price: e.target.value })
+          }
+        />
+        <input type="file" name="Artist" className="mt-4" onChange={onChange} />{" "}
+        {fileUrl && (
+          <img className="rounded mt-4" width="350px" src={fileUrl} />
+        )}
+        <button
+          onClick={createMarket}
+          className="font-bold mt-4 bg-purple-500 text-white rounded p-4 shadow-lg">
+          Mint NFT
+        </button>
+      </div>
+    </div>
+  );
 }
